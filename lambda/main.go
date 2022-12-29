13a794670dfe77892c8c6f732090f0822ba62738
oku3san <takaya0303@gmail.com>
2022-12-29 23:51:43 +0900

    Modify lambda

--- lambda/main.go
+++ lambda/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
   "context"
+  "fmt"
   "github.com/aws/aws-lambda-go/lambda"
 )
 
 func Handler(ctx context.Context) (string, error) {
+  fmt.Println("test")
   return "test run success", nil
 }
 
