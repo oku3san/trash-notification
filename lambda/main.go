a3cd74176c513f0faadf7b4f45601bc0fb0f1004
oku3san <takaya0303@gmail.com>
2022-12-31 19:46:54 +0900

    Separate struct

--- lambda/main.go
+++ lambda/main.go
@@ -8,27 +8,35 @@ import (
   "github.com/aws/aws-lambda-go/lambda"
 )
 
+type Message struct {
+  Type string `json:"type"`
+  Id   string `json:"id"`
+  Text string `json:"text"`
+}
+
+type DeliveryContext struct {
+  IsRedelivery bool `json:"isRedelivery"`
+}
+
+type Source struct {
+  Type   string `json:"type"`
+  UserId string `json:"userId"`
+}
+
+type Events []struct {
+  Type            string          `json:"type"`
+  Message         Message         `json:"message"`
+  WebhookEventId  string          `json:"webhookEventId"`
+  DeliveryContext DeliveryContext `json:"deliveryContext"`
+  Timestamp       int64           `json:"timestamp"`
+  Source          Source          `json:"source"`
+  ReplyToken      string          `json:"replyToken"`
+  Mode            string          `json:"mode"`
+}
+
 type SqsMessageFromLine struct {
   Destination string `json:"destination"`
-  Events      []struct {
-    Type    string `json:"type"`
-    Message struct {
-      Type string `json:"type"`
-      Id   string `json:"id"`
-      Text string `json:"text"`
-    } `json:"message"`
-    WebhookEventId  string `json:"webhookEventId"`
-    DeliveryContext struct {
-      IsRedelivery bool `json:"isRedelivery"`
-    } `json:"deliveryContext"`
-    Timestamp int64 `json:"timestamp"`
-    Source    struct {
-      Type   string `json:"type"`
-      UserId string `json:"userId"`
-    } `json:"source"`
-    ReplyToken string `json:"replyToken"`
-    Mode       string `json:"mode"`
-  } `json:"events"`
+  Events      Events `json:"events"`
 }
 
 func handler(ctx context.Context, sqsEvent events.SQSEvent) error {
