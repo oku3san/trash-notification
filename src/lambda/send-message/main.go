22ee43bbcc4ba0b04908e068106c95b4f6f3fef8
oku3san <takaya0303@gmail.com>
2023-01-12 23:37:07 +0900

    Print data of dynamodb stream

--- src/lambda/send-message/main.go
+++ src/lambda/send-message/main.go
@@ -2,13 +2,20 @@ package main
 
 import (
   "context"
+  "encoding/json"
   "fmt"
+  "github.com/aws/aws-lambda-go/events"
   "github.com/aws/aws-lambda-go/lambda"
 )
 
-func handler(ctx context.Context) error {
+func handler(ctx context.Context, e events.DynamoDBEvent) error {
 
   fmt.Println("send-message")
+
+  for _, record := range e.Records {
+    s, _ := json.Marshal(record.Change.NewImage)
+    fmt.Println(string(s))
+  }
 
   return nil
 }
