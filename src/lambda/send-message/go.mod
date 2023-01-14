898dc53fee0fea5f6a1e071bc0496325c73d674c
oku3san <takaya0303@gmail.com>
2023-01-14 22:55:06 +0900

    Initial settings for sending line message

--- src/lambda/send-message/go.mod
+++ src/lambda/send-message/go.mod
@@ -3,3 +3,5 @@ module send-message
 go 1.18
 
 require github.com/aws/aws-lambda-go v1.37.0
+
+require github.com/line/line-bot-sdk-go/v7 v7.18.0
--- src/lambda/send-message/main.go
+++ src/lambda/send-message/main.go
@@ -6,9 +6,23 @@ import (
   "fmt"
   "github.com/aws/aws-lambda-go/events"
   "github.com/aws/aws-lambda-go/lambda"
+  "github.com/line/line-bot-sdk-go/v7/linebot"
+  "os"
 )
 
 func handler(ctx context.Context, e events.DynamoDBEvent) error {
+  bot, err := linebot.New(
+    os.Getenv("channelSecret"),
+    os.Getenv("accessToken"),
+  )
+  userId := os.Getenv("userId")
+  //var messages []linebot.SendingMessage
+  // append some message to messages
+
+  _, err = bot.PushMessage(userId, linebot.NewTextMessage("test")).Do()
+  if err != nil {
+    fmt.Println(err)
+  }
 
   fmt.Println("send-message")
 
