25ca85e386f460d1a377f007a694768cd644caad
oku3san <takaya0303@gmail.com>
2023-01-15 23:36:20 +0900

    Update

--- src/lambda/send-message/go.mod
+++ src/lambda/send-message/go.mod
@@ -4,4 +4,9 @@ go 1.18
 
 require github.com/aws/aws-lambda-go v1.37.0
 
-require github.com/line/line-bot-sdk-go/v7 v7.18.0
+require (
+	github.com/aws/aws-sdk-go-v2/service/dynamodb v1.18.0
+	github.com/line/line-bot-sdk-go/v7 v7.18.0
+)
+
+require github.com/aws/smithy-go v1.13.5 // indirect
--- src/lambda/send-message/main.go
+++ src/lambda/send-message/main.go
@@ -2,13 +2,29 @@ package main
 
 import (
   "context"
-  "encoding/json"
   "fmt"
   "github.com/aws/aws-lambda-go/events"
   "github.com/aws/aws-lambda-go/lambda"
+  "github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
   "github.com/line/line-bot-sdk-go/v7/linebot"
+  "log"
   "os"
 )
+
+type DynamoDbEvent struct {
+  DataType struct {
+    S string `json:"S"`
+  } `json:"DataType"`
+  DataValue struct {
+    S string `json:"S"`
+  } `json:"DataValue"`
+  ID struct {
+    N string `json:"N"`
+  } `json:"Id"`
+  UpdatedAt struct {
+    N string `json:"N"`
+  } `json:"UpdatedAt"`
+}
 
 func handler(ctx context.Context, e events.DynamoDBEvent) error {
   bot, err := linebot.New(
@@ -19,16 +35,24 @@ func handler(ctx context.Context, e events.DynamoDBEvent) error {
   //var messages []linebot.SendingMessage
   // append some message to messages
 
+  fmt.Println("send-message")
+
+  for _, r := range e.Records {
+    item := make(map[string]types.AttributeValue)
+
+    for k, v := range r.Change.NewImage {
+      log.Println("arrtibute info", k, v, v.DataType())
+      if v.DataType() == events.DataTypeString {
+        item[k] = &types.AttributeValueMemberS{Value: v.String()}
+      } else if v.DataType() == events.DataTypeBoolean {
+        item[k] = &types.AttributeValueMemberBOOL{Value: v.Boolean()}
+      }
+    }
+  }
+
   _, err = bot.PushMessage(userId, linebot.NewTextMessage("test")).Do()
   if err != nil {
     fmt.Println(err)
-  }
-
-  fmt.Println("send-message")
-
-  for _, record := range e.Records {
-    s, _ := json.Marshal(record.Change.NewImage)
-    fmt.Println(string(s))
   }
 
   return nil
