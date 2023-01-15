3faaa0f8b929ade67ee96a36221036ea9a4450b1
oku3san <takaya0303@gmail.com>
2023-01-16 00:42:59 +0900

    Switch process using dynamodb stream

--- src/lambda/send-message/main.go
+++ src/lambda/send-message/main.go
@@ -5,9 +5,7 @@ import (
   "fmt"
   "github.com/aws/aws-lambda-go/events"
   "github.com/aws/aws-lambda-go/lambda"
-  "github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
   "github.com/line/line-bot-sdk-go/v7/linebot"
-  "log"
   "os"
 )
 
@@ -38,21 +36,17 @@ func handler(ctx context.Context, e events.DynamoDBEvent) error {
   fmt.Println("send-message")
 
   for _, r := range e.Records {
-    item := make(map[string]types.AttributeValue)
-
-    for k, v := range r.Change.NewImage {
-      log.Println("arrtibute info", k, v, v.DataType())
-      if v.DataType() == events.DataTypeString {
-        item[k] = &types.AttributeValueMemberS{Value: v.String()}
-      } else if v.DataType() == events.DataTypeBoolean {
-        item[k] = &types.AttributeValueMemberBOOL{Value: v.Boolean()}
+    if r.Change.NewImage["DataValue"].String() == "True" {
+      _, err = bot.PushMessage(userId, linebot.NewTextMessage("yes")).Do()
+      if err != nil {
+        fmt.Println(err)
+      }
+    } else {
+      _, err = bot.PushMessage(userId, linebot.NewTextMessage("no")).Do()
+      if err != nil {
+        fmt.Println(err)
       }
     }
-  }
-
-  _, err = bot.PushMessage(userId, linebot.NewTextMessage("test")).Do()
-  if err != nil {
-    fmt.Println(err)
   }
 
   return nil
