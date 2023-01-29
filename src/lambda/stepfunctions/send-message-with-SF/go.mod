530cdd1ae9e726dae35202ac21bc08da9fb4a229
oku3san <takaya0303@gmail.com>
2023-01-29 20:37:00 +0900

    Update Line template

--- src/lambda/stepfunctions/send-message-with-SF/go.mod
+++ src/lambda/stepfunctions/send-message-with-SF/go.mod
@@ -2,4 +2,7 @@ module send-message-with-SF
 
 go 1.18
 
-require github.com/aws/aws-lambda-go v1.37.0
+require (
+	github.com/aws/aws-lambda-go v1.37.0
+	github.com/line/line-bot-sdk-go/v7 v7.18.0
+)
--- src/lambda/stepfunctions/send-message-with-SF/main.go
+++ src/lambda/stepfunctions/send-message-with-SF/main.go
@@ -4,10 +4,31 @@ import (
   "context"
   "fmt"
   "github.com/aws/aws-lambda-go/lambda"
+  "github.com/line/line-bot-sdk-go/v7/linebot"
+  "os"
 )
 
 func handler(ctx context.Context) error {
-  fmt.Println("initial")
+  bot, err := linebot.New(
+    os.Getenv("channelSecret"),
+    os.Getenv("accessToken"),
+  )
+  if err != nil {
+    fmt.Println(err)
+  }
+  userId := os.Getenv("userId")
+
+  _, err = bot.PushMessage(userId, linebot.NewTemplateMessage(
+    "今日のゴミ捨てメッセージ",
+    linebot.NewConfirmTemplate(
+      "ゴミ捨てしましたか？",
+      linebot.NewMessageAction("はい", "はい"),
+      linebot.NewMessageAction("いいえ", "いいえ"),
+    ),
+  )).Do()
+  if err != nil {
+    fmt.Println(err)
+  }
   return nil
 }
 
